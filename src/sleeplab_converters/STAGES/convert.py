d482ff1016eb27828bf119faa5bc610f9eaf048e
Riku Huttunen <riku.huttunen@uef.fi>
2024-03-06 14:21:02 +0200

    Implemented first complete version of STAGES conversion

--- src/sleeplab_converters/STAGES/convert.py
+++ src/sleeplab_converters/STAGES/convert.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import numpy as np
 import csv
@@ -38,6 +39,7 @@ def resolve_event_start_ts(start_ts: datetime, start_str: str) -> datetime:
 def parse_csv(csvpath: Path, start_ts: datetime, epoch_sec=30.0) -> tuple[
         list[models.Annotation[models.AASMEvent]],  # Events
         list[models.Annotation[models.AASMSleepStage]],  # Hypnogram
+        list[models.Annotation[str]],  # Other annotations
         datetime | None,  # Analysis start
         datetime | None,  # Analysis end
         datetime | None,  # Lights off
@@ -52,124 +54,28 @@ def parse_csv(csvpath: Path, start_ts: datetime, epoch_sec=30.0) -> tuple[
     }
     event_map = {
         'Arousal': models.AASMEvent.AROUSAL,
-        'CentralApnea': models.AASMEvent.APNEA_CENTRAL,
-        'Desaturation': models.AASMEvent.SPO2_DESAT,
-        'Hypopnea': models.AASMEvent.HYPOPNEA,
-        'MixedApnea': models.AASMEvent.APNEA_MIXED,
+        'Arousal w/ PLM': models.AASMEvent.AROUSAL_PLM,
+        'Arousal w/ Respiratory': models.AASMEvent.AROUSAL_RES,
+        'Both Leg w/ Arousal': models.AASMEvent.AROUSAL_LM,
+        'Right Leg w/ Arousal': models.AASMEvent.AROUSAL_LM,
+        'Left Leg w/ Arousal': models.AASMEvent.AROUSAL_LM,
         'ObstructiveApnea': models.AASMEvent.APNEA_OBSTRUCTIVE,
-        'Snore': models.AASMEvent.SNORE
+        'CentralApnea': models.AASMEvent.APNEA_CENTRAL,
+        'MixedApnea': models.AASMEvent.APNEA_MIXED,
+        'Hypopnea': models.AASMEvent.HYPOPNEA,
+        'Desaturation': models.AASMEvent.SPO2_DESAT,
+        'Desaturation w/ Respiratory': models.AASMEvent.SPO2_DESAT,
+        'Snore': models.AASMEvent.SNORE,
+        'RERA': models.AASMEvent.RERA,
+        'Left Leg': models.AASMEvent.LM_LEFT,
+        'Right Leg': models.AASMEvent.LM_RIGHT,
+        'Both Leg': models.AASMEvent.LM,
+        'PLM': models.AASMEvent.PLM,
     }
-    skip_names = [
-        'Awake',
-        'CAw',
-        'CSBr',
-        'MChg',
-        'MA',
-        'MAa',
-        'mt',
-        'MT',
-        'mta',
-        'MTa',
-        'MTw',
-        'mtw',
-        'Nasal Breathing',
-        'OAw',
-        'OHw',
-        'Oral Breathing',
-        'Per Slp',
-        'REM Aw',
-        'UD1',
-        'UD1a',
-        'UD1w',
-        'UD2',
-        'UD2a',
-        'UD2w',
-        'UDAr',
-        'UDAra',
-        'Unknown',
-
-        'BIO-CALS',
-        'Lie quietly with eyes open',
-        'Lie quietly with eyes closed',
-        'Look up and down 5 times',
-        'Look left and right 5 times',
-        'Blink 5 times',
-        'Grit teeth for 5 seconds',
-        'Flex left foot 5 times',
-        'Flex right foot 5 times',
-        'Breathe through nose only for 30 seconds',
-        'Breathe through mouth only for 30 seconds',
-        'Hold breath for 10 seconds',
-        'Cough',
-        'COUGH',
-
-        'Eyes Closed',
-        'EYES CLOSED',
-        'Eyes Open',
-        'EYES OPEN',
-        'Look Up Down',
-        'EYES UP AND DOWN',
-        'Look Left Right',
-        'EYES LEFT/RIGHT',
-        'Blink 5 Times',
-        'BLINK',
-        'Grit Teeth',
-        'GRIT TEETH',
-        'Flex Left Leg',
-        'FLXL',
-        'Flex Right Leg',
-        'FLXR',
-        'NOSE',
-        'MOUTH',
-        'HOLD BREATH',
-
-        'MOVED LEFT',
-        'MOVED RIGHT',
-        'MOVED RIGHT SIDE',
-        'MOVED SUPINE',
-        'LEFT SIDE',
-        'Supine',
-
-        'EMERGENCY INTERVENTION',
-        'TIR- START CPAP',
-        'TIR TO ADJUST ORAL FLOW SENSOR',
-        'TIR TO REATTATCH HEAD LEADS AFTER PT CALLED FOR TECH TO COME REPLACE THEM',
-        'TIR TO CHECK PULSE OX',
-        'TIR TO FIX PULSE  OX',
-        'TIR FIXING CANNULA',
-        'TIR TO HELP PT UP TO THE RESTROOM- CANFLOW FIXED AT THIS TIME',
-        'TIR TO TURN TV OFF AS PT FELL ASLEEP WITHIT ON',
-        'TIR TO REPOSITION POx ON FINGER.',
-        'TIR TO HELP PT UP TO THE RESTROOM',
-        'TIR CHECKING FLOWS',
-        'TIR GETTING PT SETUP ON CPAP',
-        'TOR',
-        'TOR. FIXED CHIN',
-
-        'MACHINE CALS',
-
-        "PT'S LOUT DELAYED DUE TO NORMAL SLEEP TIME BEING ABOUT 0000 FOR CLASS HOURS.",
-
-        'Begin PT Cals',
-        'TECH AWARE OF M2',
-        'PATIENT WORRIED THAT SHE IS NOT SLEEPING',
-        'TechIn',
-        'PT to Restroom',
-        'TechOut',
-        'PATIENT STATED SHE IS READY TO END STUDY WHEN ENOUGHT TIME HAS PASSED.',
-        '0529-0548 PULSE OX NOT READING ACURATLY DUE TO SLIDING OFF A BIT',
-        'CHECK M2',
-        'REPLACED LEADS',
-        'PATIENT REQUESTING LON',
-        'IN TO FIX PULSE OX',
-        'TECH JUST REALIZED CPAP PRESSURE WAS SET AT 6CMH20 INSTEAD OF 5CMH20.',
-        'TECH DECREASED PRESSURE TO 5CMH20',
-        'INCREASED PRESSURE TO 7CMH20 FOR PHYSICIAN COMPARISON',
-        'PRESSURE INCREASED TO 9CMH20 FOR PHYSICIAN COMPARISON. TIDAL VOL 404',
-    ]
 
     hg = []
     events = []
+    other_annotations = []
     cal_times = []
 
     lights_off = None
@@ -179,9 +85,10 @@ def parse_csv(csvpath: Path, start_ts: datetime, epoch_sec=30.0) -> tuple[
         reader = csv.reader(f, delimiter=',', skipinitialspace=True)
         _ = next(reader, None)  # Skip the header line
         for row in reader:
-            if len(row) != 3:
-                logger.info(f'Wrong number of commas on row, skipping: {row}')
-                continue
+            if len(row) > 3:
+                logger.info(f'Over 3 commas on row, concatenating {row}...')
+                row = row[:2] + [', '.join(row[2:])]
+                logger.info(f'...to {row}')
             start_time_str, duration_str, name_str = row
             name_str = name_str.strip()
             event_start_ts = resolve_event_start_ts(start_ts, start_time_str)
@@ -204,26 +111,46 @@ def parse_csv(csvpath: Path, start_ts: datetime, epoch_sec=30.0) -> tuple[
             elif name_str == 'Cal':
                 # Interpret the 'cal' events as analysis_start and analysis_end
                 cal_times.append(event_start_ts)
-            elif name_str in ('LightsOff',):
-                assert lights_off is None, 'lights_off already assigned'
-                lights_off = event_start_ts
-            elif name_str in ('LightsOn', 'LON'):
-                assert lights_on is None, 'lights_on already assigned'
-                lights_on = event_start_ts
-            elif name_str in skip_names:
-                continue
+            elif (name_str in ('LightsOff',)
+                  or name_str.lower().startswith('l/o')
+                  or name_str.lower().startswith('lights out')
+                  or name_str.lower().startswith('lights off')):
+                if lights_off is not None:
+                    logger.warn('lights_off already assigned')
+                else:
+                    lights_off = event_start_ts
+            elif (name_str in ('LightsOn', 'LON')
+                  or name_str.lower().startswith('lights on')
+                  or name_str.lower().startswith('lights oon')):
+                if lights_on is not None:
+                    logger.warn('lights_on already assigned')
+                else:
+                    lights_on = event_start_ts
             else:
-                raise ValueError(f'unknown name_str: {name_str}')
+                other_annotations.append(models.Annotation[str](
+                    name=name_str,
+                    start_ts=event_start_ts,
+                    start_sec=(event_start_ts - start_ts).total_seconds(),
+                    duration=duration
+                ))
 
-    if len(cal_times) != 2:
-        logger.info(f'There should be exactly 2 Cal times, but got {cal_times}')
-        analysis_start = None
-        analysis_end = None
-    else:
+    if len(cal_times) == 2:
         analysis_start = cal_times[0]
         analysis_end = cal_times[1]
+    else:
+        if len(cal_times) > 0:
+            logger.info(f'There should be exactly 2 Cal times, but got {cal_times}')
+        analysis_start = None
+        analysis_end = None
 
-    return events, hg, analysis_start, analysis_end, lights_off, lights_on
+    # Set lights off and on to analysis_start and end, or start and end of the scored hypnogram,
+    # if annotations for them were not found.
+    if lights_off is None:
+        lights_off = analysis_start or hg[0].start_ts
+    if lights_on is None:
+        lights_on = analysis_end or hg[-1].start_ts + timedelta(seconds=hg[-1].duration)
+
+    return events, hg, other_annotations, analysis_start, analysis_end, lights_off, lights_on
 
 
 def parse_edf(edf_path: Path) -> tuple[datetime, ]:
@@ -253,25 +180,42 @@ def parse_edf(edf_path: Path) -> tuple[datetime, ]:
 
 def convert_series(src_dir: Path, series_name: str) -> models.Series:
     subjects = {}
+    error_counts = {
+        'underscore_in_name': 0,
+        'EDF_does_not_exist': 0,
+        'CSV_parse_error': 0
+    }
     # Loop through csv files
-    for csvpath in (src_dir / 'original' / 'STAGES PSGs' / 'BOGN').glob('*.csv'):
+    for csvpath in (src_dir / 'original' / 'STAGES PSGs' / series_name).glob('*.csv'):
         logger.info(f'Parsing subject {csvpath.stem}...')
         edfpath = csvpath.parent / f'{csvpath.stem}.edf'
+        subject_id = csvpath.stem
+        if '_' in edfpath.stem:
+            logger.info(f'Skipping subject with underscore in name (probably it is MSLT): {subject_id}')
+            error_counts['underscore_in_name'] += 1
+            continue
         # If there is no edf corresponding to csv, skip
         if not edfpath.exists():
             logger.info(f'EDF file matching the CSV does not exist: {edfpath}')
+            error_counts['EDF_does_not_exist'] += 1
             continue
 
-        subject_id = csvpath.stem
         start_ts, sample_arrays = parse_edf(edf_path=edfpath)
-        events, hypnogram, analysis_start, analysis_end, lights_off, lights_on = parse_csv(
-            csvpath=csvpath,
-            start_ts=start_ts
-        )
+        try:
+            events, hypnogram, other_ann, analysis_start, analysis_end, lights_off, lights_on = parse_csv(
+                csvpath=csvpath,
+                start_ts=start_ts
+            )
+        except Exception as e:
+            logger.warn(f'Skipping subject {subject_id} due to error in CSV parsing:')
+            logger.warn(e)
+            error_counts['CSV_parse_error'] += 1
+            continue
 
         annotations = {
-            'STAGES_aasmevents': models.AASMEvents(scorer='STAGES', type='aasmevents', annotations=events),
-            'STAGES_hypnogram': models.Hypnogram(scorer='STAGES', type='hypnogram', annotations=hypnogram)
+            'STAGES_aasmevents': models.AASMEvents(scorer='STAGES', annotations=events),
+            'STAGES_hypnogram': models.Hypnogram(scorer='STAGES', annotations=hypnogram),
+            'STAGES_annotations': models.Annotations(scorer='STAGES', annotations=other_ann)
         }
         metadata = models.SubjectMetadata(
             subject_id=subject_id,
@@ -291,7 +235,7 @@ def convert_series(src_dir: Path, series_name: str) -> models.Series:
 
     series = models.Series(name=series_name, subjects=subjects)
     
-    return series
+    return series, error_counts
 
 
 ALL_SERIES = [
@@ -320,12 +264,20 @@ def convert_dataset(
         clevel: int = 7,
         annotation_format: str = 'json') -> None:
     series_dict = {}
+    all_error_counts = {}
     for series_name in series:
         logger.info(f'Converting series {series_name}...')
-        series_dict[series_name] = convert_series(
+        _series, _error_counts = convert_series(
             src_dir=src_dir,
             series_name=series_name
         )
+        series_dict[series_name] = _series
+        all_error_counts[series_name] = _error_counts
+
+    error_count_path = dst_dir / 'conversion_error_counts.json'
+    logger.info(f'Writing error counts to {error_count_path}')
+    with open(error_count_path, 'w') as f:
+        json.dump(all_error_counts, f, indent=4)
 
     dataset = models.Dataset(name=ds_name, series=series_dict)
     logger.info(f'Writing dataset {ds_name} to {dst_dir}')
